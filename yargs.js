6e5bda6769cb63285b0f2b46fac49bb7b58a2f1e
unknown <buserylmz@gmail.com>
2018-11-03 13:15:54 -1200

    commit2

---
+++ yargs.js
@@ -0,0 +1,27 @@
+
+/*
+'use strict';
+
+const parseArgs = require('minimist');
+const args = require('minimist')(process.argv.slice(2));
+const yargs = require('yargs');
+const argv = yargs
+.boolean('foo',)
+
+.string('bar', )
+
+.array('number', )
+.array('hu',)
+
+console.log(args);
+//parse1  
+//>node yargs.js --foo
+//parse2
+//> node yargs.js --foo bar
+//parse3
+//> node yargs.js --number 45
+//parse4 
+//node yargs.js --foo --bar baz --number 1
+//parse5
+//node yargs.js --foo --bar baz --number 1  --bar zab
+*/
